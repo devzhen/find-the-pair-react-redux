a2616e2755dba370ccc7cbb6dc1e6ff335ce0bc6
devzhen <myjetpost@gmail.com>
2017-12-11 21:39:35 +0200

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const DEV_MODE = true;
+console.log('DEVELOPMENT MODE - ' + DEV_MODE);
+
+let config = {
+    entry: [
+        "babel-polyfill",
+        "./src/js/index.js",
+    ],
+    // devtool: ENV === "dev" ? "source-map" : false,
+    output: {
+        path: __dirname + "/dist/js",
+        filename: DEV_MODE ? 'find-the-pair.js' : 'find-the-pair.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            }
+        ]
+    },
+    plugins: [
+
+    ]
+};
+
+if (DEV_MODE) {
+
+} else {
+
+}
+
+module.exports = config;
