dbab531695c5928f43230854573b8ed1f4617762
devzhen <myjetpost@gmail.com>
2018-01-03 20:07:44 +0200

    change webpack config

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom';
 import React from "react";
+import '../css/common.css';
+import '../css/game-config.css';
+import '../css/game-field.css';
+import '../css/game-records.css';
+import '../css/game-tabs.css';
 
 import Root from "./components/root";
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 const DEV_MODE = true;
 console.log('DEVELOPMENT MODE - ' + DEV_MODE);
 
@@ -20,17 +24,33 @@ let config = {
             },
             {
                 test: /\.css$/,
-                loader: "style-loader!css-loader"
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
             }
         ]
     },
-    plugins: []
+    plugins: [
+        new UglifyJsPlugin({
+            test: /\.min.js$/,
+            exclude: /src|node_modules/,
+            sourceMap: true,
+            uglifyOptions: {
+                ecma: 8
+            }
+        })
+    ]
 };
 
 if (DEV_MODE) {
-
+    config.plugins.push(new ExtractTextPlugin("../css/find-the-pair.css"));
 } else {
-
+    config.plugins.push(new ExtractTextPlugin("../css/find-the-pair.min.css"));
+    config.plugins.push(new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.min.css$/g,
+        cssProcessorOptions: {discardComments: {removeAll: true}},
+    }));
 }
 
 module.exports = config;
